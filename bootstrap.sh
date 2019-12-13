dca05ac2375000c981ed0201b2fbeb2db85ef22e
Ruslan Manaev <manavrion@gmail.com>
2019-12-13 21:29:07 +0300

    Remove toolchain from scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@ cd "submodules/googletest/"
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../../../toolchain.cmake ..
+cmake ..
 cmake --build .
 cmake -DCMAKE_INSTALL_PREFIX=install -P cmake_install.cmake
